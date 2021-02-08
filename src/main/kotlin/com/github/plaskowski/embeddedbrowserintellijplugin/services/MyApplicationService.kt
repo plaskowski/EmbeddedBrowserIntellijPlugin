ee57f8133185233c32a330457b05551a0d6ebb84
GitHub Action <action@github.com>
2021-02-08 15:01:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/plaskowski/embeddedbrowserintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.plaskowski.embeddedbrowserintellijplugin.services
+
+import com.github.plaskowski.embeddedbrowserintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/plaskowski/embeddedbrowserintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/plaskowski/embeddedbrowserintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.plaskowski.embeddedbrowserintellijplugin.services
 
+import com.github.plaskowski.embeddedbrowserintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
